516488055982db14f837078d62b5f2aa0ac7b80f
kavon90 <kavon90@gmail.com>
2010-01-23 21:10:43 -0500

    Fixed contact info.

--- cosis/Main.java
+++ cosis/Main.java
@@ -31,7 +31,7 @@ public class Main {
     public static final String VERSION = "1.0";
     public static final String BUILD_DATE = "DEV TESTING BUILD";
     public static final String NAME = "Cosis";
-    public static final String CONTACT = "cosis.googlecode.com";
+    public static final String CONTACT = "kavon.org/cosis.htm";
     public static final String[] AUTHORS = {"Kavon Farvardin"};
 
     public static boolean WIN = false, MAC = false, UNIX = false, TRAY = true;
