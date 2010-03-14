2d48522e3ca1b30e9f16df5c3d11cc81a95554b8
kavon <kavon90@gmail.com>
2010-03-14 01:59:31 -0500

    implemented double clicking the trayicon, good night

--- cosis/gui/TrayObject.java
+++ cosis/gui/TrayObject.java
@@ -20,12 +20,57 @@ import cosis.media.Picture;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.TrayIcon;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 class TrayObject extends TrayIcon {
+
+    private final Timer timer = new Timer();
+    private boolean doubleClick = false, betweenAClick = false;
 
     TrayObject(Dimension size) {
         super(Picture.getImageIcon("cosisGIF.gif").getImage().getScaledInstance(
                 size.width, size.height, Image.SCALE_SMOOTH),
                 Main.NAME + Main.VERSION);
+
+        MouseListener mouseListener = new MouseListener() {
+
+            public void mouseClicked(MouseEvent e) {
+                int clicks = e.getClickCount();
+                if(clicks == 2) {
+                    doubleClick = true;
+                } else if(clicks == 1 && !betweenAClick) {
+                    betweenAClick = true;
+                    timer.schedule(new TimerTask() {
+
+                        @Override
+                        public void run() {
+                            if(doubleClick) {
+                                System.out.println("Double click!");
+                                doubleClick = false;
+                            } else {
+                                System.out.println("Single click!");
+                            }
+                            betweenAClick = false;
+                        }
+
+                    }, 275);
+
+                } else if(clicks >= 10) {
+                    TrayObject.this.displayMessage("Ouch!", "That hurts :(", MessageType.WARNING);
+                }
+            }
+
+            //I don't care about these actions.
+            public void mouseEntered(MouseEvent e) {}
+            public void mouseExited(MouseEvent e) {}
+            public void mousePressed(MouseEvent e) {}
+            public void mouseReleased(MouseEvent e) {}
+        };
+
+        this.addMouseListener(mouseListener);
+
     }
 }
