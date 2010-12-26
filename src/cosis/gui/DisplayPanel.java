b11c9ab127afa0542dd5a629178b26738ed48fbc
kavon <kavon90@gmail.com>
2010-12-26 01:20:09 -0500

    started with the DisplayPanel

--- src/cosis/gui/DisplayPanel.java
+++ src/cosis/gui/DisplayPanel.java
@@ -1,7 +1,48 @@
 package cosis.gui;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class DisplayPanel extends JPanel {
-
+	
+	JLabel id, pw, image, title, date;
+	JCheckBox systemTray;
+	JTextField idBox, pwBox;
+	JButton showHide;
+	Account currentAccount = null;
+	
+	
+	public DisplayPanel() {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		id = new JLabel("User ID:");
+		pw = new JLabel("Password:");
+		
+		image = new JLabel();
+		title = new JLabel();
+		date = new JLabel();
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	public void displayAccount(Account a) {
+		if(currentAccount == null) {
+			
+		}
+		
+	}
+	
+	public void setEditMode(boolean flag) {
+		
+	}
+	
 }
